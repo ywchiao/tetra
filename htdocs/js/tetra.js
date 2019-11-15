cc52ad5b79a947a3e8d0de3e6b04d73bd8b2ea3c
Yiwei Chiao <ywchiao@gmail.com>
2019-11-15 13:09:48 +0800

    class Tetromino added.

--- htdocs/js/tetra.js
+++ htdocs/js/tetra.js
@@ -1,64 +1,249 @@
 'use strict';
 
-const Board = {
-  HEIGHT: 23,
-  WIDTH: 11,
+const Game = {
+  ROWS: 29,
+  COLS: 16,
+};
+
+const gameBoard = {
   PADDING: 2,
+
+  ROW: Array(Game.COLS).fill(''),
+
+  grids: Array(Game.ROWS * Game.COLS).fill(''),
+
+  fillGrid: function (block) {
+    block.grids.forEach(grid => {
+      this.grids[grid[1] * Game.COLS + grid[0]] = block.color;
+    });
+
+    for (let r = block.top; r <= block.bottom; r++) {
+      this._clear(r);
+    }
+  },
+
+  _clear: function (row) {
+    let idx = row * Game.COLS;
+
+    if (
+      this._isFinished(this.grids.slice(idx, idx + Game.COLS))
+    ) {
+      this.grids = this.ROW.concat(
+        this.grids.slice(0, this.row * Game.COLS),
+        this.grids.slice((this.row + 1) * Game.COLs)
+      );
+    }
+  },
+
+  _isFinished: function (row) {
+    return !row.some(grid => {
+      return grid == ''
+    });
+  },
+
+  isVacant: function (block) {
+    return block.grids.every(grid => {
+      return this.grids[grid[1] * Game.COLS + grid[0]] == ''
+    });
+  },
 }
 
 const Grid = {
-  WIDTH: 20,
-  HEIGHT: 20,
+  WIDTH: 16,
+  HEIGHT: 16,
 }
 
-let block = {
-  _elapsed: 0,
+class Tetromino {
+  #left = -1;
+  #top = -1;
 
-  _color: 'peru',
+  constructor (left, top) {
+    this.#left = left;
+    this.#top = top;
+  }
 
-  _phase: 0,
+  get color() {
+    return this._color;
+  }
 
-  _left: 4,
+  get left () {
+    return this.#left;
+  }
 
-  _top: 1,
+  get top () {
+    return this.#top;
+  }
 
-  _grids: [
-    [ [0, 1],[1, 1], [2, 1], [1, 0] ],
-    [ [1, 0],[1, 1], [1, 2], [2, 1] ],
-    [ [0, 1],[1, 1], [2, 1], [1, 2] ],
-    [ [1, 0],[1, 1], [1, 2], [0, 1] ],
-  ],
+  get bottom () {
+    return this.#top + this._rows[this._phase] - 1;
+  }
 
+  get grids() {
+    return this._pattern[this._phase].map(grid => {
+      return [this.#left + grid[0], this.#top + grid[1]];
+    });
+  }
 
-  paint: function (ctx) {
-    ctx.save();
+  get cols() {
+    return this._cols[this._phase];
+  }
 
-    ctx.fillStyle = this._color;
+  moveTo(x, y) {
+    this.#left = x;
+    this.#top = y;
+  }
 
-    for (let offset of this._grids[this._phase]) {
-      ctx.fillRect(
-        (this._left + offset[0]) * Grid.WIDTH + 210 + Board.PADDING,
-        (this._top + offset[1]) * Grid.HEIGHT + Board.PADDING,
-        Grid.WIDTH, Grid.HEIGHT
-      );
-    }
+  shiftLeft () {
+    this.#left -= 1;
+  }
 
-    ctx.restore();
-  },
+  shiftRight () {
+    this.#left += 1;
+  }
 
-  update: function (delta) {
-    this._elapsed += delta
+  rotateClock () {
+    this._phase = (this._phase + 1) % this._pattern.length;
+  }
 
-    while (this._elapsed > 512) {
-      this._top = (this._top + 1) % 20;
-      this._phase = (this._phase + 1) % 4;
+  rotateCounterClock () {
+    this._phase = (this._phase - 1) % this._pattern.length;
+  }
+}
 
-      this._elapsed -= 256;
-    }
+class BlockI extends Tetromino {
+  _color = 'crimson';
+
+  _cols = [ 4, 1 ];
+  _rows = [ 1, 4 ];
+
+  _pattern = [
+    [ [0, 1],[1, 1], [2, 1], [3, 1] ],
+    [ [1, 0],[1, 1], [1, 2], [1, 3] ],
+  ];
+
+  constructor(left, top) {
+    super(left, top);
+
+    this._phase = Math.floor(Math.random() * this._pattern.length);
+  }
+};
+
+class BlockJ extends Tetromino {
+  _color = 'fuchsia';
+
+  _cols = [ 2, 3, 2, 3 ];
+  _rows = [ 3, 2, 3, 2 ];
+
+  _pattern = [
+    [ [1, 0], [1, 1], [1, 2], [0, 2] ],
+    [ [2, 1], [1, 1], [0, 1], [0, 0] ],
+    [ [0, 2], [0, 1], [0, 0], [1, 0] ],
+    [ [0, 0], [1, 0], [2, 0], [2, 1] ],
+  ];
+
+  constructor(left, top) {
+    super(left, top);
+
+    this._phase = Math.floor(Math.random() * this._pattern.length);
+  }
+};
+
+class BlockL extends Tetromino {
+  _color = 'blueviolet';
+
+  _cols = [ 2, 3, 2, 3 ];
+  _rows = [ 3, 2, 3, 2 ];
+
+  _pattern = [
+    [ [0, 0], [0, 1], [0, 2], [1, 2] ],
+    [ [2, 0], [1, 0], [0, 0], [0, 1] ],
+    [ [1, 2], [1, 1], [1, 0], [0, 0] ],
+    [ [0, 1], [1, 1], [2, 1], [2, 0] ],
+  ];
+
+  constructor(left, top) {
+    super(left, top);
+
+    this._phase = Math.floor(Math.random() * this._pattern.length);
+  }
+};
+
+class BlockO extends Tetromino {
+  _color = 'cyan';
+
+  _cols = [ 2 ];
+  _rows = [ 2 ];
+
+  _pattern = [
+    [ [0, 0],[1, 0], [0, 1], [1, 1] ],
+  ];
+
+  constructor (left, top) {
+    super(left, top);
+
+    this._phase = Math.floor(Math.random() * this._pattern.length);
+  }
+};
+
+class BlockS extends Tetromino {
+  _color = 'dodgerblue';
+
+  _cols = [ 3, 2 ];
+  _rows = [ 2, 3 ];
+
+  _pattern = [
+    [ [2, 0], [1, 0], [1, 1], [0, 1] ],
+    [ [0, 0], [0, 1], [1, 1], [1, 2] ],
+  ];
+
+  constructor (left, top) {
+    super(left, top);
+
+    this._phase = Math.floor(Math.random() * this._pattern.length);
+  }
+};
+
+class BlockT extends Tetromino {
+  _color = 'peru';
+
+  _cols = [ 3, 2, 3, 2 ];
+  _rows = [ 2, 3, 2, 3 ];
+
+  _pattern = [
+    [ [0, 1], [1, 1], [2, 1], [1, 0] ],
+    [ [0, 0], [0, 1], [0, 2], [1, 1] ],
+    [ [0, 0], [1, 0], [2, 0], [1, 1] ],
+    [ [1, 0], [1, 1], [1, 2], [0, 1] ],
+  ];
+
+  constructor (left, top) {
+    super(left, top);
+
+    this._phase = Math.floor(Math.random() * this._pattern.length);
+  }
+};
+
+class BlockZ extends Tetromino {
+  _color = 'lime';
+
+  _cols = [ 3, 2 ];
+  _rows = [ 2, 3 ];
+
+  _pattern = [
+    [ [0, 0], [1, 0], [1, 1], [2, 1] ],
+    [ [1, 0], [1, 1], [0, 1], [0, 2] ],
+  ];
+
+  constructor (left, top) {
+    super(left, top);
+
+    this._phase = Math.floor(Math.random() * this._pattern.length);
   }
 };
 
 let game = {
+  _elapsed: 0,
+
   _loop: function (ticks) {
     if (!this._startAt) {
       this._startAt = ticks;
@@ -70,8 +255,18 @@ let game = {
     requestAnimationFrame(this._loop.bind(this));
   },
 
+  _nextBlock: function () {
+    let blocks = [
+      BlockI, BlockJ, BlockL, BlockO, BlockS, BlockT, BlockZ,
+    ];
+
+    return new blocks[Math.floor(Math.random() * blocks.length)]();
+  },
+
   // 重繪 *遊戲盤面*
   _paint: function () {
+    let block = this._block;
+
     // 取得能在 canvas 上繪圖的 context2d 物件
     let ctx = document.querySelector('canvas').getContext('2d');
 
@@ -86,12 +281,72 @@ let game = {
     ctx.strokeRect(208, 4, 224, 472);
     ctx.strokeRect(438, 4, 198, 472);
 
-    block.paint(ctx);
+    //gameBoard.paint(ctx);
+    this._paintBoard(ctx);
+    this._paintBlock(ctx);
+  },
+
+  _paintBoard: function (ctx) {
+    ctx.save();
+
+    gameBoard.grids.forEach( (color, i) => {
+      if (color != '') {
+        ctx.fillStyle = color;
+
+        ctx.fillRect(
+          (i % Game.COLS) * Grid.WIDTH + 210 + gameBoard.PADDING,
+          (Math.floor(i / Game.COLS)) * Grid.HEIGHT + 6 + gameBoard.PADDING,
+          Grid.WIDTH, Grid.HEIGHT
+        );
+      }
+    });
+
+    ctx.restore();
+  },
+
+  _paintBlock: function (ctx) {
+    let block = this._block;
+
+    ctx.save();
+
+    ctx.fillStyle = block.color;
+
+    block.grids.forEach(grid => {
+      ctx.fillRect(
+        grid[0] * Grid.WIDTH + 210 + gameBoard.PADDING,
+        grid[1] * Grid.HEIGHT + 6 + gameBoard.PADDING,
+        Grid.WIDTH, Grid.HEIGHT
+      );
+    });
+
+    ctx.restore();
+  },
+
+  _translate: function (grids) {
   },
 
   _update: function (ticks) {
     if (this._lastUpdate) {
-      block.update(ticks - this._lastUpdate);
+      this._elapsed += (ticks - this._lastUpdate);
+
+      while (this._elapsed > 512) {
+        let block = this._block;
+
+        block.moveTo(block.left, block.top + 1);
+
+        if (block.bottom == gameBoard.HEIGHT || !gameBoard.isVacant(block)) {
+          block.moveTo(block.left, block.top - 1);
+
+          gameBoard.fillGrid(block);
+
+          this._block = this._next;
+          this._next = this._nextBlock();
+
+          this._block.moveTo(Math.floor((Game.COLS - this._block.cols) / 2) - 1, 0);
+        }
+
+        this._elapsed -= 512;
+      }
     }
 
     this._lastUpdate = ticks;
@@ -102,6 +357,9 @@ let game = {
   },
 
   start: function () {
+    this._block = this._nextBlock();
+    this._next = this._nextBlock();
+
     this._tickHandler = requestAnimationFrame(this._loop.bind(this));
   },
 };

e3af1b04e74edec356c4f3522aec3750e445e057
Yiwei Chiao <ywchiao@gmail.com>
2019-12-06 12:57:06 +0800

    httpd/index.js added.

---
+++ httpd/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let http = require('http');
+
+http.createServer((request, response) => {
+  // 傳送 HTTP header
+  // HTTP Status:  200 : OK
+  // Content Type: text/plain
+  response.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
+
+  // 傳送回應內容。
+  response.end('Hello World!\n');
+
+  console.log('request.headers: \n', request.headers);
+}).listen(8088);
+
+// log message to Console
+console.log('  伺服器啟動，連線 url: http://127.0.0.1:8088/');
+
+// index.js
